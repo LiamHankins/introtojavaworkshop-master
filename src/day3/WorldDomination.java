a0d56625edc52cdd2d44f15ee4614ccf941bc1c3
Love4653 <league@WTS5.attlocal.net>
2016-08-12 11:37:45 -0700

    Failure

---
+++ src/day3/WorldDomination.java
@@ -0,0 +1,17 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class WorldDomination {
+	public static void main(String[] args) {
+		// 1. Ask the user if they know how to write code.
+String idk= JOptionPane.showInputDialog("Do you like Blobfish?");
+		// 2. If they say "yes", tell them they will rule the world.
+if( idk.equals("yes")){ System.out.println("Good."); 
+
+}else{ System.out.println("Blobfish!!");}
+		// 3. Otherwise, wish them good luck washing dishes.
+
+	}
+}
+
---
+++ src/publicClassDay3.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class publicClassDay3 {
+ public static void main(String[] args) {
+	 String ans = "Blobfish!!";
+	 JOptionPane.showMessageDialog(null, ans);
+String name=	 JOptionPane.showInputDialog("CrazyBlobfish");
+System.out.println(name);
+
+}
+
+}
